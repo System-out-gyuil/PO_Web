8602fdc0cc644a8df4f5131642334a9e9969a015
System-out-gyuil <a46884334@gmail.com>
2025-05-02 16:10:07 +0900

    add search categori

--- main/views.py
+++ main/views.py
@@ -11,13 +11,25 @@ class MainView(View):
         return render(request, 'main/main.html')
 
     def post(self, request):
-        region = request.POST.get('region')
-        industry = request.POST.get('industry')
+        data = {
+            "region": request.POST.get("region", ""),
+            "industry": request.POST.get("industry", ""),
+            "business_period": request.POST.get("business-period", ""),
+            "export": request.POST.get("export", ""),
+            "sales_volume": request.POST.get("sales-volume", ""),
+            "member_number": request.POST.get("member-number", ""),
+        }
 
-        if region and industry:
-            return redirect(f'/search/?region={region}&industry={industry}')
+        print(data)
+
+        # 적어도 region + industry는 필수로 요구
+        if data["region"] and data["industry"]:
+            # 파라미터를 GET으로 넘겨줌
+            query_string = "&".join([f"{k}={v}" for k, v in data.items() if v])
+            return redirect(f"/search/?{query_string}")
         else:
             return render(request, 'main/main.html')
+
 
 
 class SearchResultView(View):
