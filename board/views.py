70704d9406158128d42823d5814c92bcd6490b8e
System-out-gyuil <a46884334@gmail.com>
2025-05-13 11:00:00 +0900

    publishing v1

--- board/views.py
+++ board/views.py
@@ -18,6 +18,59 @@ class BoardView(View):
 
         has_next = page_index < total_pages
 
+        # views.py
+
+        REGIONS = [
+            '서울특별시', '부산광역시', '대구광역시', '인천광역시', '광주광역시', '대전광역시', '울산광역시',
+            '세종특별자치시', '경기도', '강원특별자치도', '충청북도', '충청남도', '전북특별자치도', '전라남도',
+            '경상북도', '경상남도', '제주특별자치도', '경북', '경남', '전북', '전남', '인천', '충북', '충남'
+        ]
+
+        REGION_MAP = {
+            '서울특별시': '서울',
+            '부산광역시': '부산',
+            '대구광역시': '대구',
+            '인천광역시': '인천',
+            '광주광역시': '광주',
+            '대전광역시': '대전',
+            '울산광역시': '울산',
+            '세종특별자치시': '세종',
+            '경기도': '경기',
+            '강원특별자치도': '강원',
+            '충청북도': '충북',
+            '충청남도': '충남',
+            '전북특별자치도': '전북',
+            '전라남도': '전남',
+            '경상북도': '경북',
+            '경상남도': '경남',
+            '제주특별자치도': '제주',
+        }
+
+        # '경북', '전북' 등 약어도 포함 (이미 약어인 경우를 위해)
+        REGION_SHORTS = list(REGION_MAP.values()) + ['경북', '경남', '전북', '전남', '충북', '충남', '인천']
+
+        for item in items:
+            # 긴 이름 매핑 먼저 확인
+            found = False
+            for full, short in REGION_MAP.items():
+                if full in item.institution_name:
+                    item.region = short
+                    found = True
+                    break
+
+            if not found:
+                # 약어가 institution_name 안에 그대로 포함돼 있다면 그대로 사용
+                for short in REGION_SHORTS:
+                    if short in item.institution_name:
+                        item.region = short
+                        found = True
+                        break
+
+            if not found:
+                item.region = '전국'
+
+
+
         # 페이징 블록 계산
         page_block = 10
         block_start = ((page_index - 1) // page_block) * page_block + 1
