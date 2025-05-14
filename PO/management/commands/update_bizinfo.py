259036937e33540ca54fca5cf63431dcb81859c3
System-out-gyuil <a46884334@gmail.com>
2025-05-14 15:08:20 +0900

    path change

--- PO/management/commands/update_bizinfo.py
+++ PO/management/commands/update_bizinfo.py
@@ -54,7 +54,7 @@ class Command(BaseCommand):
                 iframe_src = fetch_iframe_src(pblanc_id, CHROME_DRIVER_PATH)
 
                 # 파일 처리
-                file_url = item.get("flpthNm")
+                file_url = item.get("printFlpthNm")
                 raw_file_name = item.get("printFileNm") or "default.pdf"
                 file_name = self.sanitize_filename(raw_file_name)
                 text, structured_data = "", {}
@@ -179,7 +179,7 @@ class Command(BaseCommand):
         prompt = (
             "아래 텍스트는 정부 지원사업 공고문에서 추출된 실제 내용입니다. "
             "이 내용을 기반으로 허구 없이 정확하게 요약해줘. 추가적인 추론이나 가정은 하지 말고, 원문 기반으로만 작성해줘.\n\n"
-            "\ud83d\udccc 아래 항목들을 정확히 JSON 형식으로 추출해줘\n"
+            "아래 항목들을 정확히 JSON 형식으로 추출해줘\n"
             "- 직원수 : 무관, 1~4인, 5인 이상 (중 선택, 복수 선택 가능)\n"
             "- 매출규모: 무관, 1억 이하, 1~5억, 5~10억, 10~30억, 30억 이상 (중 선택, 복수 선택 가능)\n"
             "- 공고내용: 최소 450자 이상, 원문 기반 요약\n\n"
