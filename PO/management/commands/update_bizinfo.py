ba7f20d42e3942e776127a949ff929a02b72e863
System-out-gyuil <a46884334@gmail.com>
2025-05-20 13:25:21 +0900

    test 18

--- PO/management/commands/update_bizinfo.py
+++ PO/management/commands/update_bizinfo.py
@@ -28,7 +28,7 @@ class Command(BaseCommand):
             "dataType": "json",
             "searchCnt": 100,
             "pageUnit": 20,
-            "pageIndex": 3
+            "pageIndex": 1
         }
 
         try:
