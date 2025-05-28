e7a0c99ded67b5f5e20cabe96bd45630105ba33c
System-out-gyuil <a46884334@gmail.com>
2025-05-28 15:23:20 +0900

    industry data change, industry css modified

--- open_ai_test.py
+++ open_ai_test.py
@@ -25,36 +25,20 @@ from django.db.models import Q
 # ✅ Django 설정 초기화
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PO.settings")
 django.setup()
+from main.models import Industry
 
-from board.models import BizInfo
 
-region = "경북"
-employee = "5인 이상"
-possible_industry = "제조업"
-sales = "10억~30억"
-export = "수출 실적 보유"
-date = "3년 이상"
-#   & Q(employee_count__contains=employee) & Q(revenue__contains=sales) & Q(business_period__contains=date)
-data = BizInfo.objects.filter(
-    (Q(region__contains=region) | Q(region__contains="전국")) & Q(possible_industry__contains=possible_industry) & Q(employee_count__contains=employee)
-)
+data = Industry.objects.all()
 
 datas = ''
 
 for i in data:
-  if i.noti_summary:
-    datas += f'id: {i.id}, title:{i.title}, summary:{i.noti_summary}, region:{i.region}\n\n'
+  datas += f'대분류:{i.big_category} 소분류:{i.small_category},\n'
 
 # print(data)
 print(datas)
 
-text = f'\n사업지 주소지 {region}이고, 
-        대분류: 곡물 가공품 제조업, 소분류: 기타 곡물 가공품 제조업을 영위함, 
-        개업한지 3년 4개월 되었고 작년 10억 매출에 수출 실적이 있어, 직원은 7명이야 
-        아래 지원 공고 내용을 토대로 선정 가능성이 높은 공고를 알려줘, 지역과 업종을 고려해서 판단해줘
-        적합도 점수(자사의 정보로 선정될 수 있는) 100점 만점으로 해서 우선순위를 정해줘, 80점 이상의 공고만 보여줘
-        가중치를 설정해줄게 
-        공고 id, 공고 제목을 포함해서 보여줘\n'
+text = f'{datas} \n 목욕탕 운영에 적합한 업종 5개 제시, 대분류 소분류 기준으로 말해'
 
 llm = ChatOpenAI(
     temperature=0,
