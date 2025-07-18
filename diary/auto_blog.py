0a295663c8be2cab4b9f688f543479c8202c9884
System-out-gyuil <a46884334@gmail.com>
2025-07-18 10:51:48 +0900

    auto blog tkinter remove

--- diary/auto_blog.py
+++ diary/auto_blog.py
@@ -9,8 +9,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import os
-import tkinter as tk
-from tkinterdnd2 import DND_FILES, TkinterDnD
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -18,7 +16,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 import traceback
-from tkinter import filedialog, messagebox
 import time
 import random
 from selenium.webdriver.common.keys import Keys
