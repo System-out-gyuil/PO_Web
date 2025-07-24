fb21c7aa4fad24823664d6b54ed671935f93ac32
System-out-gyuil <a46884334@gmail.com>
2025-07-24 17:22:20 +0900

    noti btn

--- diary/main_views.py
+++ diary/main_views.py
@@ -3,4 +3,5 @@ from django.views import View
 
 class DiaryMainView(View):
     def get(self, request):
-        return render(request, 'diary/diary_main.html')
+        is_authenticated = request.session.get('diary_authenticated', False)
+        return render(request, 'diary/diary_main.html', {'is_authenticated': is_authenticated})
